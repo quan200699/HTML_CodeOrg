b5b200324413b063e7f8e1bae8dcabcba89918bc
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-09-17 21:37:19 +0700

    add pig image

--- pig.js
+++ pig.js
@@ -1,10 +1,9 @@
-function Pig (x, y) {
+function Pig(x, y) {
     this.x = x;
     this.y = y;
+    this.image = new Image();
     this.drawPig = (ctx) => {
-        ctx.beginPath();
-        ctx.fillStyle = "#08ff00";
-        ctx.fillRect(x, y, 20, 20);
-        ctx.stroke();
+        this.image.src = "img/goalIdle.gif"
+        ctx.drawImage(this.image, this.x, this.y,40,40);
     }
 }
