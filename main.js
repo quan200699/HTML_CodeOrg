870f7d180f40f90317a614c351b0ecbd998a19c2
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-09-17 14:45:51 +0700

    test drag and drop

--- main.js
+++ main.js
@@ -3,6 +3,3 @@ let pig = new Pig(200, 200);
 let gameBoard = new GameBoard(CANVAS_ID, GAME_BOARD_WIDTH, GAME_BOARD_HEIGHT);
 gameBoard.drawGameBoard();
 gameBoard.drawGame(bird, pig);
-$( function() {
-    $( "#draggable" ).draggable();
-} );
