b1292c3ac450f471d575636171e8dd5819d2a2cd
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-09-18 13:47:44 +0700

    change canvas size

--- js/general-variable.js
+++ js/general-variable.js
@@ -1,4 +1,4 @@
 const GAME_PLAY_ID = 'game-play';
 const CANVAS_ID = 'game-board';
-const GAME_BOARD_WIDTH = window.innerWidth / 4;
-const GAME_BOARD_HEIGHT = window.innerHeight * 0.4;
+const GAME_BOARD_WIDTH = 400;
+const GAME_BOARD_HEIGHT = 400;
--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 let bird = new Bird(200, 10);
-let pig = new Pig(200, 200);
+let pig = new Pig(200, 100);
 let gameBoard = new GameBoard(CANVAS_ID, GAME_BOARD_WIDTH, GAME_BOARD_HEIGHT);
 gameBoard.drawGameBoard();
 gameBoard.drawGame(bird, pig);
