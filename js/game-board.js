6bc79536e02dbc8de3bb33380802ab36b9b0ba81
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-09-18 15:32:56 +0700

    add image to project

--- js/game-board.js
+++ js/game-board.js
@@ -2,14 +2,19 @@ function GameBoard(id, width, height) {
     this.id = id;
     this.width = width;
     this.height = height;
+    this.image = new Image();
     this.drawGameBoard = () => {
         let draw = "";
-        draw += `<canvas id="${this.id}" width="${this.width}" height="${this.height}" 
-                    style="border: black; background: bisque;"></canvas>`
+        draw += `<canvas id="${this.id}" width="${this.width}" height="${this.height}"></canvas>`
         document.getElementById(GAME_PLAY_ID).innerHTML = draw;
+    }
+    this.drawBackground = (ctx) => {
+        this.image.src = "img/background.png"
+        ctx.drawImage(this.image, 0, 0);
     }
     this.drawGame = (bird, pig) => {
         let ctx = document.getElementById(this.id).getContext("2d");
+        this.drawBackground(ctx)
         bird.drawBird(ctx);
         pig.drawPig(ctx);
     }
--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-let bird = new Bird(200, 10);
-let pig = new Pig(200, 100);
+let bird = new Bird(200, 150);
+let pig = new Pig(200, 250);
 let gameBoard = new GameBoard(CANVAS_ID, GAME_BOARD_WIDTH, GAME_BOARD_HEIGHT);
 gameBoard.drawGameBoard();
 gameBoard.drawGame(bird, pig);
